943d4f514cef92968c08dd8690e61f02e8c237eb
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2022-06-09 15:45:52 +0800

    feat: 新增支援 gif 圖片上傳及修改限制2MB

--- services/isValidImage.js
+++ services/isValidImage.js
@@ -4,15 +4,16 @@ const AppError = require('./appError');
 const handleErrorAsync = require('./handleErrorAsync');
 const upload = multer({
   limits: {
-    fileSize: 1 * 1024 * 1024, // 上傳限制 1MB
+    fileSize: 2 * 1024 * 1024, // 上傳限制 2MB
   },
   fileFilter(req, file, cb) {
     const ext = path.extname(file.originalname).toLowerCase();
-    if (ext !== '.jpg' && ext !== '.png' && ext !== '.jpeg') {
+    if (ext !== '.jpg' && ext !== '.png' && ext !== '.jpeg' && ext !== '.gif') {
       cb(
         new AppError({
           statusCode: 400,
-          message: '[上傳錯誤] 檔案格式錯誤：僅限上傳 jpg、jpeg 與 png 格式。',
+          message:
+            '[上傳錯誤] 檔案格式錯誤：僅限上傳 jpg、jpeg、png 與 gif 格式。',
         })
       );
     }
